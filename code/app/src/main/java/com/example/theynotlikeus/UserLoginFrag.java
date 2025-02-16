4a9790eb43d777e7d698d9f27ad7e524a0ac1eab
NYM <101108705+nymur-rahman-nyar@users.noreply.github.com>
2025-02-16 13:16:33 -0700

    Added working back button to the UserLoginFrag

--- code/app/src/main/java/com/example/theynotlikeus/UserLoginFrag.java
+++ code/app/src/main/java/com/example/theynotlikeus/UserLoginFrag.java
@@ -2,11 +2,18 @@ package com.example.theynotlikeus;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +66,25 @@ public class UserLoginFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
+
         return inflater.inflate(R.layout.fragment_user_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Find button
+        MaterialToolbar backButton = view.findViewById(R.id.button_userloginfrag_back);
+        NavController navController = Navigation.findNavController(view);
+        backButton.setOnClickListener(v ->
+                navController.navigate(R.id.action_userLoginFrag_to_loginUserSelectionFrag)
+        );
+
+
+
     }
 }
