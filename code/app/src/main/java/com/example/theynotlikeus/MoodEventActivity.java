9f6acc3d5533d46885826f2a390007dd8a71b4ec
ercelangeles <156619970+ercelangeles@users.noreply.github.com>
2025-02-19 15:47:03 -0700

    updated the moodeventactivity in this branch

--- code/app/src/main/java/com/example/theynotlikeus/MoodEventActivity.java
+++ code/app/src/main/java/com/example/theynotlikeus/MoodEventActivity.java
@@ -17,7 +17,7 @@ public class MoodEventActivity extends AppCompatActivity {
             finish(); // Closes activity and returns to previous screen
         });
 
-        findViewById(R.id.imagebutton_activitymoodeventdetails_backbutton).setOnClickListener(v -> finish()); // Back to previous screen when you hit back button
+        findViewById(R.id.button_activitymoodevent_backbutton).setOnClickListener(v -> finish()); // Back to previous screen when you hit back button
     }
 }
 
