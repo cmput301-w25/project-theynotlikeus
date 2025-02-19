d83316258206e61247c2fe435d5336496b04fd1f
ap001AP <approjects8473@gmail.com>
2025-02-19 14:39:46 -0700

    Fixed the delete edit fragment class

--- code/app/src/main/java/com/example/theynotlikeus/DeleteEditMoodFrag.java
+++ code/app/src/main/java/com/example/theynotlikeus/DeleteEditMoodFrag.java
@@ -36,8 +36,8 @@ public class DeleteEditMoodFrag extends Fragment {
      * @return A new instance of fragment DeleteEditMood.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeMapFrag newInstance(String param1, String param2) {
-        HomeMapFrag fragment = new HomeMapFrag();
+    public static DeleteEditMoodFrag newInstance(String param1, String param2) {
+        DeleteEditMoodFrag fragment = new DeleteEditMoodFrag();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,6 +58,6 @@ public class DeleteEditMoodFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend_request, container, false);
+        return inflater.inflate(R.layout.fragment_delete_edit_mood, container, false);
     }
 }
