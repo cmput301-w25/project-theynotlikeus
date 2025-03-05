839552a62902a1ce6bbf5dbffbc6f2b8a469e331
subaita <subaita1@ualberta.ca>
2025-03-05 14:11:13 -0700

    Deleted unwanted files

--- code/app/src/test/java/com/example/theynotlikeus/MainActivityTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.theynotlikeus;
-
-public class MainActivityTest {
-}
--- code/app/src/test/java/com/example/theynotlikeus/PersonalProfileDetailsActivityTest.java
+++ code/app/src/test/java/com/example/theynotlikeus/PersonalProfileDetailsActivityTest.java
@@ -1,4 +1,6 @@
 package com.example.theynotlikeus;
 
 public class PersonalProfileDetailsActivityTest {
+    //Implement unit tests when the activity is implemented
+    //Update UI tests for this activity after its implementation
 }
