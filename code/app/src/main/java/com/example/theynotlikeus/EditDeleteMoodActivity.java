8f67da28e5dae470999d1d52637a365c7bf28b12
subaita <subaita1@ualberta.ca>
2025-03-05 00:17:44 -0700

    final commit

--- code/app/src/main/java/com/example/theynotlikeus/EditDeleteMoodActivity.java
+++ code/app/src/main/java/com/example/theynotlikeus/EditDeleteMoodActivity.java
@@ -2,6 +2,7 @@ package com.example.theynotlikeus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
