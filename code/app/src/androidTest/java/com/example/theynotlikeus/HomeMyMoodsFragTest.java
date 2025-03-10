d4abe20b516fabe1f7e32c6e197669d228198439
NYM <101108705+nymur-rahman-nyar@users.noreply.github.com>
2025-03-10 04:07:44 -0600

    fragment open test passed

--- code/app/src/androidTest/java/com/example/theynotlikeus/HomeMyMoodsFragTest.java
+++ code/app/src/androidTest/java/com/example/theynotlikeus/HomeMyMoodsFragTest.java
@@ -1,4 +1,27 @@
 package com.example.theynotlikeus;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class HomeMyMoodsFragTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testHomeMyMoodsFragmentIsDisplayedByDefault() {
+        // Verify that the HomeMyMoodsFragment is displayed by default upon launching MainActivity
+        onView(withId(R.id.recyclerview_HomeMyMoodsFragment_userrecyclerview))
+                .check(matches(isDisplayed()));
+    }
 }
